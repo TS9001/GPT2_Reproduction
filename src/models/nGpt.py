7758ecd4c11de955d697c23606ac3d10f1a0a799
tsikora <tomassikora@proton.me>
2025-02-05 19:42:04 +0100

    Fixed ngpt

--- src/models/nGpt.py
+++ src/models/nGpt.py
@@ -10,7 +10,7 @@ from torch.nn import functional as F
 
 class Attention(nn.Module):
     def __init__(self, config: ModelConfiguration):
-        super().__init__(config)
+        super().__init__()
         self.c_attn = nn.Linear(config.d_model, config.d_model*3)
         self.c_proj = nn.Linear(config.d_model, config.d_model)
         self.c_proj.NANOGPT_SCALE_INIT = 1
